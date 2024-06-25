cbf4e014cdf3be0fafd0405d62fc49b3c69cb2bd
Ravish Bisht <ravishbisht85@gmail.com>
2024-06-25 07:57:46 +0530

    database connection done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("hell ravish");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8001;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection error", err);
+  });
