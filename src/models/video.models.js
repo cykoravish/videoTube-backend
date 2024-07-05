9c497da9ba2ffffb89aa7b9515a3c93474a59309
Ravish Bisht <ravishbisht85@gmail.com>
2024-07-05 09:41:49 +0530

    video model add

---
+++ src/models/video.models.js
@@ -0,0 +1,55 @@
+/*
+ owners object id
+ videoFile string
+ thumbnail string
+ title string
+ description string,
+ duration string,
+ views number
+ views number
+ isPublished boolean
+ createdAt date
+ updatedAt Date
+ */
+
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
